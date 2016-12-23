fefef4cd8831d65e6074dd09b4dc9b5506bdcd85
Buck Doyle <b@chromatin.ca>
2016-12-23 02:29:10 -0500

    Add placeholder text on index route

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PrisonRideshare.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "More to come."
   end
 end
