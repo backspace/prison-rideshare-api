af1eff589f77430988148acaa07124fa2248fbc0
Buck Doyle <b@chromatin.ca>
2016-12-24 01:21:11 -0500

    Add institution extraction during import

--- lib/mix/tasks/import.ex
+++ lib/mix/tasks/import.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Import do
   use Mix.Task
 
-  alias PrisonRideshare.{Repo, Request}
+  alias PrisonRideshare.{Institution, Repo, Request}
 
   @shortdoc "Imports CSVs"
 
@@ -14,14 +14,28 @@ defmodule Mix.Tasks.Import do
 
     File.stream!(requests)
     |> CSV.decode
-    |> Enum.with_index
-    |> Enum.each(fn {row, i} ->
+    |> Stream.with_index
+    |> Enum.reduce(%{}, fn({row, i}, institution_name_to_model) ->
       if i > 0 do
-        [_, _, _, _, _, address | _] = row
+        [_, _, institution, _, _, address | _] = row
+        matching_institution = String.downcase(institution)
+
+        institution_name_to_model = Map.put_new_lazy(institution_name_to_model, matching_institution, fn ->
+          Institution.changeset(%Institution{}, %{name: institution, rate: 25})
+          |> Repo.insert!
+        end)
+
+        institution_model = Map.get(institution_name_to_model, matching_institution)
+
         request_attrs = Map.put(valid_attrs, :address, address)
+        |> Map.put(:institution_id, institution_model.id)
 
         Request.changeset(%Request{}, request_attrs)
         |> Repo.insert!
+
+        institution_name_to_model
+      else
+        institution_name_to_model
       end
     end)
   end
--- test/mix/tasks/import_test.exs
+++ test/mix/tasks/import_test.exs
@@ -2,14 +2,24 @@ defmodule Mix.Tasks.ImportTest do
   use ExUnit.Case
   use PrisonRideshare.ConnCase
 
-  alias PrisonRideshare.{Repo, Request}
+  alias PrisonRideshare.{Institution, Repo, Request}
 
   test "something" do
     Mix.Tasks.Import.run "test/support/import/requests.csv"
 
-    [req1, req2] = Repo.all(Request)
+    [i1, i2] = Repo.all(Institution)
+
+    assert i1.name == "Headingley"
+    assert i2.name == "stony mountain"
+
+    [req1, req2, req3] = Repo.all(Request)
 
     assert req1.address == "91 Albert"
+    assert req1.institution_id == i1.id
+
     assert req2.address == "114 Spence"
+    assert req2.institution_id == i2.id
+
+    assert req3.institution_id == i2.id
   end
 end
