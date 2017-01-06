7288fc09650a3b939446610ecea8eb0c949b965c
Buck Doyle <b@chromatin.ca>
2017-01-06 13:27:11 -0500

    Remove unused routes and pipelines

--- web/router.ex
+++ web/router.ex
@@ -1,40 +1,8 @@
 defmodule PrisonRideshare.Router do
   use PrisonRideshare.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :protected do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :admin do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json-api"]
-  end
-
-  scope "/" do
-    pipe_through :browser
-  end
-
-  scope "/" do
-    pipe_through :protected
   end
 
   scope "/", PrisonRideshare do
@@ -48,9 +16,4 @@ defmodule PrisonRideshare.Router do
     resources "/reimbursements", ReimbursementController, except: [:new, :edit]
     resources "/rides", RideController, except: [:new, :edit]
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PrisonRideshare do
-  #   pipe_through :api
-  # end
 end
