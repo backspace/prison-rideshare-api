fc61e99ace046c48467772c8e1955e81e3353585
Buck Doyle <b@chromatin.ca>
2017-08-22 13:41:41 -0500

    Add generated audit code
    This will probably require tweaking but this is what came from
    running the following:
    mix whatwasit.install --whodoneit --whodoneit-id-type=uuid

---
+++ priv/repo/migrations/20170822183819_create_whatwasit_version.exs
@@ -0,0 +1,15 @@
+defmodule PrisonRideshare.Repo.Migrations.CreateWhatwasitVersion do
+  use Ecto.Migration
+  def change do
+    create table(:versions) do
+      add :item_type, :string, null: false
+      add :item_id, :integer, null: false
+      add :action, :string
+      add :object, :map, null: false
+      add :whodoneit_name, :string
+      add :whodoneit_id, references(:users, on_delete: :nilify_all, type: :uuid)
+      timestamps
+    end
+
+  end
+end
