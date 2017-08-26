a092a82c0b70d27f65d75417aa751b391d70d99c
Buck Doyle <b@chromatin.ca>
2017-08-25 20:15:22 -0500

    Add fallback for audit with no user
    Without this, the IP doesn’t get stored…?

--- lib/prison_rideshare/whodoneit.ex
+++ lib/prison_rideshare/whodoneit.ex
@@ -1,6 +1,6 @@
 defmodule PrisonRideshare.WhoDoneIt do
   def whodoneit(conn) do
     user = Guardian.Plug.current_resource(conn)
-    [whodoneit: user, whodoneit_name: to_string(:inet_parse.ntoa(conn.remote_ip))]
+    [whodoneit: user || :none, whodoneit_name: to_string(:inet_parse.ntoa(conn.remote_ip))]
   end
 end
