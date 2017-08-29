b451471416818c69f84527ba4be9b5948aee5595
Buck Doyle <b@chromatin.ca>
2017-08-28 22:00:08 -0500

    Add import auditing

--- test/mix/tasks/import_test.exs
+++ test/mix/tasks/import_test.exs
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.ImportTest do
   test "something" do
     Mix.Tasks.Import.run ["test/support/import/requests.csv", "test/support/import/reports.csv", "test/support/import/reimbursements.csv"]
 
-    [i1version, _i2v] = Repo.all(Version)
+    [i1version, p1version, p2version, req1version, i2version, p3version, _rcw2, _r2, _r3, _r4, _r5, _r6, _r7, _r8, _r9, combiningVersion, reportVersion1, reportVersion2, reiVersion1, _rei2, _rei3, _rei4] = Repo.all(Version)
 
     [i1, i2] = Repo.all(Institution)
 
@@ -43,13 +43,25 @@ defmodule Mix.Tasks.ImportTest do
     assert req1.car_owner.name == "Oliver Gathing"
     assert req1.enabled
 
+    assert req1version.origin == "import"
+    assert req1version.event == "insert"
+    assert req1version.item_changes["address"] == "91 Albert"
+    assert req1version.item_id == req1.id
+    assert req1version.item_type == "Ride"
+
     assert req1.distance == 75
     assert req1.rate == ~M[25]
     assert req1.food_expenses == ~M[1200]
     assert req1.car_expenses == ~M[2625]
     assert req1.report_notes == "These R the Notes"
 
+    assert reportVersion1.event == "update"
+    assert reportVersion1.item_changes["report_notes"] == "These R the Notes"
+
     assert reqCombinedWith2.combined_with_ride_id == req2.id
+
+    assert combiningVersion.event == "update"
+    assert combiningVersion.item_changes["combined_with_ride_id"] == req2.id
 
     assert req2.address == "114 Spence"
     assert req2.institution_id == i2.id
@@ -80,8 +92,12 @@ defmodule Mix.Tasks.ImportTest do
     [p1, p2, p3] = Repo.all(Person)
     [rei1, rei2, rei3, rei4] = Repo.all(Reimbursement)
 
+    assert p1version.item_changes["name"] == "Lucy Parsons"
+
     assert rei1.person_id == p1.id
     assert rei1.food_expenses == ~M[1200]
+
+    assert reiVersion1.item_changes["person_id"] == p1.id
 
     assert rei2.person_id == p2.id
     assert rei2.car_expenses == ~M[2625]
