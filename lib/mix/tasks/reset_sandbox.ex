f48976012bea527dc6ea0762187a570665197749
Buck Doyle <b@chromatin.ca>
2017-11-11 11:23:39 -0600

    Add documentation for sandbox task
    This is needed for it to be recognised as a task!

--- lib/mix/tasks/reset_sandbox.ex
+++ lib/mix/tasks/reset_sandbox.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.ResetSandbox do
   use Mix.Task
 
+  @shortdoc "Reset the database with sandbox data"
+
   def run(_) do
     Mix.Task.run "ecto.rollback", ["--all"]
     Mix.Task.run "ecto.migrate"
