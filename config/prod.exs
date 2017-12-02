4db4c286b146dfbd7238aaffbea695825286bbcf
Buck Doyle <b@chromatin.ca>
2017-12-02 12:35:19 -0600

    Remove origin-checking

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :prison_rideshare, PrisonRideshareWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  check_origin: ["https://" <> System.get_env("ORIGIN_HOST")],
+  check_origin: false,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
