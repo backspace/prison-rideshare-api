4c0903b9950e8412aa871a5ec09699b2ac568177
Buck Doyle <b@chromatin.ca>
2018-01-10 22:00:29 +0100

    Add block on working with past slots
    This includes a much-needed refactoring of the hideous nested
    conditionals 🎉

--- lib/prison_rideshare_web/controllers/commitment_controller.ex
+++ lib/prison_rideshare_web/controllers/commitment_controller.ex
@@ -8,57 +8,73 @@ defmodule PrisonRideshareWeb.CommitmentController do
 
   def create(conn, %{"data" => data = %{"type" => "commitments"}}) do
     person = PrisonRideshare.PersonGuardian.Plug.current_resource(conn)
+    slot = Repo.get!(Slot, data["relationships"]["slot"]["data"]["id"])
+    |> Repo.preload(:commitments)
 
     cond do
-      person.id == data["relationships"]["person"]["data"]["id"] ->
-        slot = Repo.get!(Slot, data["relationships"]["slot"]["data"]["id"])
-        |> Repo.preload(:commitments)
-
-        if slot.count == 0 || length(slot.commitments) + 1 < slot.count do
-          if Enum.any?(slot.commitments, fn(commitment) -> commitment.person_id == person.id end) do
-            conn
-            |> put_status(:unprocessable_entity)
-            |> render(:errors, data:
-              [%{
-                :detail => "Person is already committed to this slot",
-                :source => %{"pointer" => "/data/relationships/slot"},
-                :title => "is already committed-to"
-              }]
-            )
-          else
-            changeset = Commitment.changeset(%Commitment{}, Params.to_attributes(data))
-
-            case PaperTrail.insert(changeset, version_information(conn)) do
-              {:ok, %{model: commitment}} ->
-                conn
-                |> put_status(:created)
-                |> put_resp_header("location", commitment_path(conn, :show, commitment))
-                |> render("show.json-api", data: commitment |> Repo.preload([:person, :slot]))
-            end
-          end
-        else
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render(:errors, data:
-            [%{
-              :detail => "Slot has its maximum number of commitments",
-              :source => %{"pointer" => "/data/relationships/slot"},
-              :title => "is full"
-            }]
-          )
-        end
-      true ->
+      person.id != data["relationships"]["person"]["data"]["id"] ->
         conn
         |> put_status(:unauthorized)
         |> render(PrisonRideshareWeb.ErrorView, "401.json")
+      slot.count != 0 && length(slot.commitments) + 1 >= slot.count ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data:
+          [%{
+            :detail => "Slot has its maximum number of commitments",
+            :source => %{"pointer" => "/data/relationships/slot"},
+            :title => "is full"
+          }]
+        )
+      Enum.any?(slot.commitments, fn(commitment) -> commitment.person_id == person.id end) ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data:
+          [%{
+            :detail => "Person is already committed to this slot",
+            :source => %{"pointer" => "/data/relationships/slot"},
+            :title => "is already committed-to"
+          }]
+        )
+      Timex.before?(slot.start, Timex.now) ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data:
+          [%{
+            :detail => "Cannot commit to a past slot",
+            :source => %{"pointer" => "/data/relationships/slot"},
+            :title => "is in the past"
+          }]
+        )
+      true ->
+        changeset = Commitment.changeset(%Commitment{}, Params.to_attributes(data))
+
+        case PaperTrail.insert(changeset, version_information(conn)) do
+          {:ok, %{model: commitment}} ->
+            conn
+            |> put_status(:created)
+            |> put_resp_header("location", commitment_path(conn, :show, commitment))
+            |> render("show.json-api", data: commitment |> Repo.preload([:person, :slot]))
+          end
     end
   end
 
   def delete(conn, %{"id" => id}) do
     commitment = Repo.get!(Commitment, id)
+    |> Repo.preload(:slot)
     person = PrisonRideshare.PersonGuardian.Plug.current_resource(conn)
 
     cond do
+      Timex.before?(commitment.slot.start, Timex.now) ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data:
+          [%{
+            :detail => "Cannot delete a past commitment",
+            :source => %{"pointer" => "/data/relationships/slot"},
+            :title => "is in the past"
+          }]
+        )
       person.id == commitment.person_id ->
         PaperTrail.delete!(commitment, version_information(conn))
         send_resp(conn, :no_content, "")
