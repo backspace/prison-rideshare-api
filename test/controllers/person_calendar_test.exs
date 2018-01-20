0f8b167d14fc8ea04b59a9233610d0ea08937484
Buck Doyle <b@chromatin.ca>
2018-01-20 12:57:16 +0100

    Add more auto-formatting

--- test/controllers/person_calendar_test.exs
+++ test/controllers/person_calendar_test.exs
@@ -5,7 +5,8 @@ defmodule PrisonRideshare.UnauthRideControllerTest do
   alias PrisonRideshare.Repo
 
   setup do
-    conn = build_conn()
+    conn =
+      build_conn()
       |> put_req_header("accept", "text/calendar")
       |> put_req_header("content-type", "text/calendar")
 
@@ -13,82 +14,84 @@ defmodule PrisonRideshare.UnauthRideControllerTest do
   end
 
   test "lists calendar events for a driver's rides", %{conn: conn} do
-    institution = Repo.insert! %Institution{name: "Stony Mountain"}
-    driver = Repo.insert! %Person{name: "Chelsea Manning"}
+    institution = Repo.insert!(%Institution{name: "Stony Mountain"})
+    driver = Repo.insert!(%Person{name: "Chelsea Manning"})
 
-    ride = Repo.insert! %Ride{
-      start: Ecto.DateTime.from_erl({{2017, 1, 15}, {18, 0, 0}}),
-      end: Ecto.DateTime.from_erl({{2017, 1, 15}, {20, 0, 0}}),
-      institution: institution,
-      driver: driver,
-      name: "Tom",
-      address: "421 Osborne",
-      contact: "2877433"
-    }
+    ride =
+      Repo.insert!(%Ride{
+        start: Ecto.DateTime.from_erl({{2017, 1, 15}, {18, 0, 0}}),
+        end: Ecto.DateTime.from_erl({{2017, 1, 15}, {20, 0, 0}}),
+        institution: institution,
+        driver: driver,
+        name: "Tom",
+        address: "421 Osborne",
+        contact: "2877433"
+      })
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       combined_with: ride,
       name: "Tina",
       address: "414 Osborne",
       contact: "287878"
-    }
+    })
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       start: Ecto.DateTime.from_erl({{2017, 1, 16}, {18, 0, 0}}),
       end: Ecto.DateTime.from_erl({{2017, 1, 16}, {20, 0, 0}}),
       enabled: false,
       institution: institution,
       driver: driver
-    }
+    })
 
     # Repo.insert! %Ride{
     #   distance: 77
     # }
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       start: Ecto.DateTime.from_erl({{2017, 1, 17}, {18, 0, 0}}),
       end: Ecto.DateTime.from_erl({{2017, 1, 17}, {20, 0, 0}}),
       institution: institution,
       driver: driver,
       address: "421 Osborne"
-    }
+    })
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       name: "Tom",
       address: "421 Osborne",
       contact: "2877433"
-    }
+    })
 
     # FIXME should add to description when child ride doesn’t match parent times
 
-    conn = get conn, person_path(conn, :calendar, driver.id)
+    conn = get(conn, person_path(conn, :calendar, driver.id))
     assert response_content_type(conn, :calendar)
+
     assert response(conn, 200) == """
-    BEGIN:VCALENDAR
-    CALSCALE:GREGORIAN
-    VERSION:2.0
-    BEGIN:VEVENT
-    DESCRIPTION:Tom\\n421 Osborne\\n2877433\\n\\n\\nTina\\n414 Osborne\\n287878\\n
-    DTEND;TZID=Etc/UTC:20170115T200000
-    DTSTART;TZID=Etc/UTC:20170115T180000
-    LOCATION:421 Osborne\\, 414 Osborne
-    SUMMARY:Visit to Stony Mountain
-    END:VEVENT
-    BEGIN:VEVENT
-    DESCRIPTION:\\n\\n\\n
-    DTEND;TZID=Etc/UTC:20170116T200000
-    DTSTART;TZID=Etc/UTC:20170116T180000
-    LOCATION:
-    SUMMARY:CANCELLED Visit to Stony Mountain
-    END:VEVENT
-    BEGIN:VEVENT
-    DESCRIPTION:\\n421 Osborne\\n\\n
-    DTEND;TZID=Etc/UTC:20170117T200000
-    DTSTART;TZID=Etc/UTC:20170117T180000
-    LOCATION:421 Osborne
-    SUMMARY:Visit to Stony Mountain
-    END:VEVENT
-    END:VCALENDAR
-    """
+           BEGIN:VCALENDAR
+           CALSCALE:GREGORIAN
+           VERSION:2.0
+           BEGIN:VEVENT
+           DESCRIPTION:Tom\\n421 Osborne\\n2877433\\n\\n\\nTina\\n414 Osborne\\n287878\\n
+           DTEND;TZID=Etc/UTC:20170115T200000
+           DTSTART;TZID=Etc/UTC:20170115T180000
+           LOCATION:421 Osborne\\, 414 Osborne
+           SUMMARY:Visit to Stony Mountain
+           END:VEVENT
+           BEGIN:VEVENT
+           DESCRIPTION:\\n\\n\\n
+           DTEND;TZID=Etc/UTC:20170116T200000
+           DTSTART;TZID=Etc/UTC:20170116T180000
+           LOCATION:
+           SUMMARY:CANCELLED Visit to Stony Mountain
+           END:VEVENT
+           BEGIN:VEVENT
+           DESCRIPTION:\\n421 Osborne\\n\\n
+           DTEND;TZID=Etc/UTC:20170117T200000
+           DTSTART;TZID=Etc/UTC:20170117T180000
+           LOCATION:421 Osborne
+           SUMMARY:Visit to Stony Mountain
+           END:VEVENT
+           END:VCALENDAR
+           """
   end
 end
--- test/controllers/ride_calendar_test.exs
+++ test/controllers/ride_calendar_test.exs
@@ -5,7 +5,8 @@ defmodule PrisonRideshare.RideCalendarTest do
   alias PrisonRideshare.Repo
 
   setup do
-    conn = build_conn()
+    conn =
+      build_conn()
       |> put_req_header("accept", "text/calendar")
       |> put_req_header("content-type", "text/calendar")
 
@@ -13,10 +14,10 @@ defmodule PrisonRideshare.RideCalendarTest do
   end
 
   test "lists unfilled and not-cancelled ride events", %{conn: conn} do
-    institution = Repo.insert! %Institution{name: "Stony Mountain"}
-    driver = Repo.insert! %Person{name: "Chelsea Manning"}
+    institution = Repo.insert!(%Institution{name: "Stony Mountain"})
+    driver = Repo.insert!(%Person{name: "Chelsea Manning"})
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       start: Ecto.DateTime.from_erl({{2017, 1, 14}, {18, 0, 0}}),
       end: Ecto.DateTime.from_erl({{2017, 1, 14}, {20, 0, 0}}),
       institution: institution,
@@ -24,62 +25,64 @@ defmodule PrisonRideshare.RideCalendarTest do
       passengers: 1,
       address: "421 Osborne",
       contact: "2877433"
-    }
+    })
 
-    ride = Repo.insert! %Ride{
-      start: Ecto.DateTime.from_erl({{2017, 1, 15}, {18, 0, 0}}),
-      end: Ecto.DateTime.from_erl({{2017, 1, 15}, {20, 0, 0}}),
-      institution: institution,
-      name: "Tom",
-      passengers: 2,
-      address: "421 Osborne",
-      contact: "2877433"
-    }
+    ride =
+      Repo.insert!(%Ride{
+        start: Ecto.DateTime.from_erl({{2017, 1, 15}, {18, 0, 0}}),
+        end: Ecto.DateTime.from_erl({{2017, 1, 15}, {20, 0, 0}}),
+        institution: institution,
+        name: "Tom",
+        passengers: 2,
+        address: "421 Osborne",
+        contact: "2877433"
+      })
 
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       combined_with: ride,
       name: "Tina",
       passengers: 1,
       address: "414 Osborne",
       contact: "287878"
-    }
+    })
 
     # This ride should not be visible since it was cancelled.
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       start: Ecto.DateTime.from_erl({{2017, 1, 16}, {18, 0, 0}}),
       end: Ecto.DateTime.from_erl({{2017, 1, 16}, {20, 0, 0}}),
       enabled: false,
       institution: institution
-    }
+    })
 
     # This ride should not be visible since it has a driver.
-    Repo.insert! %Ride{
+    Repo.insert!(%Ride{
       start: Ecto.DateTime.from_erl({{2017, 1, 17}, {18, 0, 0}}),
       end: Ecto.DateTime.from_erl({{2017, 1, 17}, {20, 0, 0}}),
       institution: institution,
       driver: driver,
       address: "421 Osborne"
-    }
+    })
 
-    conn = get conn, ride_path(conn, :calendar)
+    conn = get(conn, ride_path(conn, :calendar))
     assert response_content_type(conn, :calendar)
+
     assert response(conn, 200) == """
-    BEGIN:VCALENDAR
-    CALSCALE:GREGORIAN
-    VERSION:2.0
-    BEGIN:VEVENT
-    DESCRIPTION:Please email barnone.coordinator@gmail.com to get assigned to this ride.
-    DTEND;TZID=Etc/UTC:20170114T200000
-    DTSTART;TZID=Etc/UTC:20170114T180000
-    SUMMARY:Stony Mountain: 1 request
-    END:VEVENT
-    BEGIN:VEVENT
-    DESCRIPTION:Please email barnone.coordinator@gmail.com to get assigned to this ride.
-    DTEND;TZID=Etc/UTC:20170115T200000
-    DTSTART;TZID=Etc/UTC:20170115T180000
-    SUMMARY:Stony Mountain: 2 requests, 3 passengers
-    END:VEVENT
-    END:VCALENDAR
-    """
+           BEGIN:VCALENDAR
+           CALSCALE:GREGORIAN
+           VERSION:2.0
+           BEGIN:VEVENT
+           DESCRIPTION:Please email barnone.coordinator@gmail.com to get assigned to this ride.
+           DTEND;TZID=Etc/UTC:20170114T200000
+           DTSTART;TZID=Etc/UTC:20170114T180000
+           SUMMARY:Stony Mountain: 1 request
+           END:VEVENT
+           BEGIN:VEVENT
+           DESCRIPTION:Please email barnone.coordinator@gmail.com to get assigned to this ride.
+           DTEND;TZID=Etc/UTC:20170115T200000
+           DTSTART;TZID=Etc/UTC:20170115T180000
+           SUMMARY:Stony Mountain: 2 requests, 3 passengers
+           END:VEVENT
+           END:VCALENDAR
+           """
   end
 end
