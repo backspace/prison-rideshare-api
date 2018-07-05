ea9e941e59622478a578a66b5c4d6fcfff427daa
Buck Doyle <b@chromatin.ca>
2018-07-05 11:46:29 -0500

    Add institution far-ness to migration
    This is pretty specific… 🤔

--- priv/repo/migrations/20180624203812_add_institution_far.exs
+++ priv/repo/migrations/20180624203812_add_institution_far.exs
@@ -5,5 +5,9 @@ defmodule PrisonRideshare.Repo.Migrations.AddInstitutionFar do
     alter table(:institutions) do
       add(:far, :boolean)
     end
+
+    execute("""
+      update institutions set far = true where rate = 20
+    """)
   end
 end
