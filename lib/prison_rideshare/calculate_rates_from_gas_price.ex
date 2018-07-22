fdaddc96642e8ecf3b34518e879d938e3a416c11
Buck Doyle <b@chromatin.ca>
2018-07-22 12:32:03 -0500

    Remove unused variable

--- lib/prison_rideshare/calculate_rates_from_gas_price.ex
+++ lib/prison_rideshare/calculate_rates_from_gas_price.ex
@@ -4,9 +4,8 @@ defmodule PrisonRideshare.CalculateRatesFromGasPrice do
       Money.multiply(gas_price.price, 17.5)
       |> Money.add(2.4)
 
-    rate =
-      (rate_money.amount / 100)
-      |> round
+    (rate_money.amount / 100)
+    |> round
   end
 
   def close_rate(gas_price) do
