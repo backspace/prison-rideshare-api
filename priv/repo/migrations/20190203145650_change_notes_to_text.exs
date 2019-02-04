48b3f4c214525bb90a8f1ef5f2adf87868d0fc8b
Buck Doyle <b@chromatin.ca>
2019-02-04 16:53:34 -0600

    Change notes text migration to be reversible

--- priv/repo/migrations/20190203145650_change_notes_to_text.exs
+++ priv/repo/migrations/20190203145650_change_notes_to_text.exs
@@ -1,9 +1,15 @@
 defmodule PrisonRideshare.Repo.Migrations.ChangeNotesToText do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:rides) do
       modify(:request_notes, :text)
+    end
+  end
+
+  def down do
+    alter table(:rides) do
+      modify(:request_notes, :string)
     end
   end
 end
