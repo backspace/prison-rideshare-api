d8046026c5b786b11300b40e772b89e56023e36d
Buck Doyle <b@chromatin.ca>
2019-03-17 13:57:17 -0700

    Add chmod for known_hosts
    As described here:
    https://docs.gitlab.com/ee/ci/ssh_keys/

--- .travis/deploy-sandbox.sh
+++ .travis/deploy-sandbox.sh
@@ -4,6 +4,7 @@ chmod 600 .travis/deploy.key # this key should have push access
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add .travis/deploy.key
 ssh-keyscan corepoint.chromatin.ca > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 
 git remote add deploy dokku@corepoint.chromatin.ca:prison-rideshare-api-sandbox
 git config --global push.default simple
