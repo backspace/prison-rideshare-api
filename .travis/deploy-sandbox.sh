021127b216f19b2beb6e66c40247bbb78f8d0eda
Buck Doyle <b@chromatin.ca>
2019-03-17 13:32:56 -0700

    Add deployment step
    This is my attempt to replicate the Travis file-encryption
    feature, let’s see…

--- .travis/deploy-sandbox.sh
+++ .travis/deploy-sandbox.sh
@@ -1,4 +1,4 @@
-openssl aes-256-cbc -K $encrypted_efc605b46718_key -iv $encrypted_efc605b46718_iv -in .travis/deploy.key.enc -out .travis/deploy.key -d
+echo $DEPLOYMENT_KEY > .travis/deploy.key
 chmod 600 .travis/deploy.key # this key should have push access
 
 eval "$(ssh-agent -s)" #start the ssh agent
