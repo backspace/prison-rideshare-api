579ac9fd7a1216ab33ada301602235a469345edc
Buck Doyle <b@chromatin.ca>
2019-03-19 10:07:14 -0700

    Remove incorrect comment

--- .ci/deploy-sandbox.sh
+++ .ci/deploy-sandbox.sh
@@ -10,7 +10,6 @@ chmod 644 ~/.ssh/known_hosts
 
 git remote add deploy dokku@corepoint.chromatin.ca:prison-rideshare-api-sandbox
 git config --global push.default simple
-# FIXME change branch
 git push deploy HEAD:primary
 
 ssh -t dokku@corepoint.chromatin.ca -- run prison-rideshare-api-sandbox mix reset_sandbox
