909c57ba8cd4f17dce61c797433a8a200d683160
Buck Doyle <b@chromatin.ca>
2019-03-19 10:06:58 -0700

    Remove extraneous whitespace

--- .ci/deploy-sandbox.sh
+++ .ci/deploy-sandbox.sh
@@ -6,7 +6,6 @@ eval "$(ssh-agent -s)" #start the ssh agent
 echo "$DEPLOYMENT_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 ssh-keyscan corepoint.chromatin.ca >> ~/.ssh/known_hosts
-
 chmod 644 ~/.ssh/known_hosts
 
 git remote add deploy dokku@corepoint.chromatin.ca:prison-rideshare-api-sandbox
