e1d48e4ae0693bada02e22f832697510613a76d0
Buck Doyle <b@chromatin.ca>
2022-12-20 15:17:28 -0500

    Fix warnings in custom type
    embed_as wasn’t needed after switching to use instead of
    behaviour.

--- lib/prison_rideshare/secret.ex
+++ lib/prison_rideshare/secret.ex
@@ -1,5 +1,5 @@
 defmodule PrisonRideshare.Secret do
-  @behaviour Ecto.Type
+  use Ecto.Type
 
   def type, do: :string
 
@@ -14,8 +14,6 @@ defmodule PrisonRideshare.Secret do
   def dump(string) do
     {:ok, string}
   end
-
-  def embed_as(format), do: :self
 
   def autogenerate do
     :crypto.strong_rand_bytes(32) |> Base.encode64() |> binary_part(0, 32)
