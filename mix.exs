7375f6ac4ff46785eecd13e74d1ae5bf07c56ad5
Buck Doyle <b@chromatin.ca>
2023-02-15 17:41:58 -0600

    Update ExCoveralls version

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule PrisonRideshare.Mixfile do
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.0"},
       {:paper_trail, "~> 0.14"},
-      {:excoveralls, "~> 0.6", only: :test},
+      {:excoveralls, "~> 0.15", only: :test},
       {:faker, "~> 0.17"},
       {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
       {:mock, "~> 0.3.4", only: :test},
